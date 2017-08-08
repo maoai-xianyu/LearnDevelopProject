66ca848f5bcfae120a107e450a28d8c601784f90
maoai_xianyu <194264514@qq.com>
2017-08-08 18:49:45 +0800

    网路请求添加成功，准备Rxjava

---
+++ app/src/main/java/com/mao/cn/learnDevelopProject/ui/features/IRxJavaLearn.java
@@ -0,0 +1,19 @@
+// +----------------------------------------------------------------------
+// | Project:   LearnMyDevelopProject
+// +----------------------------------------------------------------------
+// | CreateTime: 08/08/2017 18:35 下午
+// +----------------------------------------------------------------------
+// | Author:     xab(xab@xabad.cn)
+// +----------------------------------------------------------------------
+// | Description:
+// +----------------------------------------------------------------------
+package com.mao.cn.learnDevelopProject.ui.features;
+
+import com.mao.cn.learnDevelopProject.ui.commons.BaseViewInferface;
+
+/**
+* DESC   :
+* AUTHOR : Xabad
+*/
+public interface IRxJavaLearn extends BaseViewInferface{
+}
