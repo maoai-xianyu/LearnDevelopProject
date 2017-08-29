6f025af15f2d01c3aa568687ede93abb1237b578
maoai_xianyu <194264514@qq.com>
2017-08-29 20:22:05 +0800

    Rxjava interval 是一个时间轮训

--- app/src/main/java/com/mao/cn/learnDevelopProject/ui/activity/LoadingActivity.java
+++ app/src/main/java/com/mao/cn/learnDevelopProject/ui/activity/LoadingActivity.java
@@ -23,6 +23,7 @@ import com.mao.cn.learnDevelopProject.modules.LoadingModule;
 import com.mao.cn.learnDevelopProject.ui.commons.BaseActivity;
 import com.mao.cn.learnDevelopProject.ui.features.ILoading;
 import com.mao.cn.learnDevelopProject.ui.presenter.LoadingPresenter;
+import com.mao.cn.learnDevelopProject.utils.tools.LogU;
 
 import java.util.concurrent.TimeUnit;
 
@@ -30,6 +31,8 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import rx.Observable;
+import rx.Subscription;
+import rx.functions.Action1;
 
 /**
  * DESC   :
@@ -44,6 +47,7 @@ public class LoadingActivity extends BaseActivity implements ILoading {
     SimpleDraweeView ivLoadingBackground;
     @BindView(R.id.tv_show)
     TextView tvShow;
+    private Subscription subscribe;
 
     @Override
     public void getArgs(Bundle bundle) {
@@ -67,7 +71,25 @@ public class LoadingActivity extends BaseActivity implements ILoading {
     @Override
     public void initView() {
 
-        Observable.interval(5,TimeUnit.SECONDS).compose(timer()).subscribe(aLong -> startActivity(MainActivity.class, true));
+        subscribe = Observable.interval(5, TimeUnit.SECONDS).compose(timer()).subscribe(new Action1<Long>() {
+            @Override
+            public void call(Long aLong) {
+
+                LogU.i("  aLong  " + aLong);
+                startActivity(MainActivity.class, true);
+            }
+        });
+
+
+        // timer  定时
+        /*Observable.timer(5, TimeUnit.SECONDS).compose(timer()).subscribe(new Action1<Long>() {
+            @Override
+            public void call(Long aLong) {
+                LogU.i("  aLong  " + aLong);
+                startActivity(MainActivity.class, true);
+            }
+        });*/
+
 
         //当应用是使用系统安装器安装并且运行的时候，category中是没有任何信息的，这个时候会导致按home键后，点击图标重启app。
         /*if (!isTaskRoot()) {
@@ -80,6 +102,7 @@ public class LoadingActivity extends BaseActivity implements ILoading {
         }*/
 
     }
+
     @Override
     public void setListener() {
 
@@ -91,5 +114,15 @@ public class LoadingActivity extends BaseActivity implements ILoading {
                 .appComponent(appComponent)
                 .loadingModule(new LoadingModule(this))
                 .build().inject(this);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (subscribe != null && !subscribe.isUnsubscribed()) {
+            subscribe.unsubscribe();
+        }
+
     }
 }
