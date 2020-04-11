38c923e477282a9b0df777a3c1694bdada210a55
zhangkun@boxfish.cn <194264514@qq.com>
2020-04-11 18:27:01 +0800

    拖拽的时候实现复用

--- app/src/main/java/com/mao/cn/learnDevelopProject/ui/fragment/materialDesign/drag/ItemTouchHelperCallback.java
+++ app/src/main/java/com/mao/cn/learnDevelopProject/ui/fragment/materialDesign/drag/ItemTouchHelperCallback.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.mao.cn.learnDevelopProject.R;
 
 public class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
+
     private ItemTouchMoveListener moveListener;
 
     public ItemTouchHelperCallback(ItemTouchMoveListener moveListener) {
@@ -74,6 +75,11 @@ public class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
     public void clearView(@NonNull RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
         // 恢复
         viewHolder.itemView.setBackgroundColor(Color.WHITE);
+
+        // 方案一 复用问题恢复
+       /* viewHolder.itemView.setAlpha(1);//1~0
+        viewHolder.itemView.setScaleX(1);//1~0
+        viewHolder.itemView.setScaleY(1);//1~0*/
         super.clearView(recyclerView, viewHolder);
     }
 
@@ -82,13 +88,30 @@ public class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
                             @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState,
                             boolean isCurrentlyActive) {
         // dX:水平方向移动的增量（负：往左；正：往右）范围：0~View.getWidth  0~1
+        //透明度动画
+        float alpha = 1 - Math.abs(dX) / viewHolder.itemView.getWidth();
         if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
-            //透明度动画
-            float alpha = 1 - Math.abs(dX) / viewHolder.itemView.getWidth();
+
             viewHolder.itemView.setAlpha(alpha);//1~0
             viewHolder.itemView.setScaleX(alpha);//1~0
             viewHolder.itemView.setScaleY(alpha);//1~0
         }
+        // 方案二
+        /*if (alpha == 0) {
+            viewHolder.itemView.setAlpha(1);//1~0
+            viewHolder.itemView.setScaleX(1);//1~0
+            viewHolder.itemView.setScaleY(1);//1~0
+        }*/
+
+
+        // qq 效果 滑动到一半， 判断是否超出或者达到 width/2 就让其 setTranslationX 设置一半的位置  有问题
+        /*if (Math.abs(dX) <= viewHolder.itemView.getWidth() / 2) {
+            viewHolder.itemView.setTranslationX(-0.5f * viewHolder.itemView.getWidth());
+        } else {
+            viewHolder.itemView.setTranslationX(dX);
+        }*/
+
+
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState,
                 isCurrentlyActive);
     }
@@ -99,14 +122,16 @@ public class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
         /**
          * 当拖拽的时候回调
          * 可以在此方法里面实现：拖拽条目并实现刷新效果
+         *
          * @param fromPosition 从什么位置拖
-         * @param toPosition	到什么位置
+         * @param toPosition   到什么位置
          * @return 是否执行了move  可以不用返回值 在 onMove 直接返回 true
          */
         boolean onItemMove(int fromPosition, int toPosition);
 
         /**
          * 当条目被移除是回调
+         *
          * @param position 移除的位置
          * @return
          */
