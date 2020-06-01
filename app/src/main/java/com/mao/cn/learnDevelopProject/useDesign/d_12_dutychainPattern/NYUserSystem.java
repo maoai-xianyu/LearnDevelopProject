ef2b37f79a30be25096a3f31f5227c3d50875936
zhangkun@boxfish.cn <194264514@qq.com>
2020-06-01 22:26:16 +0800

    责任链设计模式

---
+++ app/src/main/java/com/mao/cn/learnDevelopProject/useDesign/d_12_dutychainPattern/NYUserSystem.java
@@ -0,0 +1,42 @@
+package com.mao.cn.learnDevelopProject.useDesign.d_12_dutychainPattern;
+
+
+import com.mao.cn.learnDevelopProject.useDesign.d_12_dutychainPattern.handler.AbsUserSystemHandler;
+import com.mao.cn.learnDevelopProject.useDesign.d_12_dutychainPattern.handler.UserInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhangkun
+ * @time 2020-05-28 15:52
+ * @Description
+ */
+public class NYUserSystem extends AbsUserSystemHandler {
+
+    private List<UserInfo> mUserInfos;
+
+    public NYUserSystem() {
+        mUserInfos = new ArrayList<>();
+        mUserInfos.add(new UserInfo("ny大南","123456","1","男"));
+        mUserInfos.add(new UserInfo("ny大丹","123456","2","男"));
+        mUserInfos.add(new UserInfo("ny大二","123456","13","男"));
+    }
+
+
+    @Override
+    public UserInfo getUserInfo(String name, String psd) {
+        for (UserInfo next : mUserInfos) {
+            if (next.userName.equals(name) && next.userPsd.equals(psd)) {
+                return next;
+            }
+        }
+
+        AbsUserSystemHandler nextHandler = getNextHandler();
+        if (nextHandler != null){
+            return nextHandler.getUserInfo(name,psd);
+        }
+
+        return null;
+    }
+}
